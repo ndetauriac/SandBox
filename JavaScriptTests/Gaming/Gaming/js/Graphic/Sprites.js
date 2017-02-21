fe12b454a562622a3dad6eb09f9b260c7b48fdf8
ndetauriac <nicolas.detauriac@gmail.com>
2017-02-22 00:55:23 +0100

    Gaming: Creation of class sprites
    Signed-off-by: ndetauriac <nicolas.detauriac@gmail.com>

---
+++ JavaScriptTests/Gaming/Gaming/js/Graphic/Sprites.js
@@ -0,0 +1,24 @@
+﻿class Sprites
+{
+    constructor(img, frameNumber, x, y)
+    {
+        this.context2D = document.getElementById('gameArea').getContext('2d');
+        this.imgSprite = new Image();
+        this.imgSprite.src = img;
+        this.spriteW = this.imgSprite.width / frameNumber;
+        this.spriteH = this.imgSprite.height;
+        this.imgSprite.style.position = 'relative';
+        this.posX = x;
+        this.posY = y;
+        this.frame = 0;
+    }
+
+    draw()
+    {
+        this.context2D.clearRect(this.posX, this.posY, this.spriteW, this.spriteH);
+        if (this.imgSprite != undefined)
+            this.context2D.drawImage(this.imgSprite, this.frame * this.spriteW, 0, this.spriteW, this.spriteH, this.posX, this.posY, this.spriteW, this.spriteH);
+
+        this.frame = (this.frame + 1) % 4;
+    }
+}
---
+++ JavaScriptTests/Gaming/Gaming/js/player.js
@@ -0,0 +1,51 @@
+﻿class Player {
+    constructor() {
+        this.imgPlayer = new Image();
+        this.imgPlayer.src = "images/playerSprites.jpg";
+        this.imgPlayer.style.position = 'relative';
+        this.imgPlayer.style.left = '0px';
+        this.imgPlayer.style.top = '0px';
+        this.posX = 0;
+        this.posY = 0;
+        this.lastMove = "NONE";
+    }
+
+    updatePosition() {
+        switch (this.lastMove) {
+            case "UP":
+                this.moveUp();
+                break;
+            case "DOWN":
+                this.moveDown();
+                break;
+            case "LEFT":
+                this.moveLeft();
+                break;
+            case "RIGHT":
+                this.moveRight();
+                break;
+            default:
+                break;
+        }
+    }
+
+    setMove(move) {
+        this.lastMove = move;
+    }
+
+    moveUp() {
+        this.imgPlayer.style.top = parseInt(this.imgPlayer.style.top) - 10 + 'px';
+    }
+
+    moveDown() {
+        this.imgPlayer.style.top = parseInt(this.imgPlayer.style.top) + 10 + 'px';
+    }
+
+    moveLeft() {
+        this.imgPlayer.style.left = parseInt(this.imgPlayer.style.left) - 10 + 'px';
+    }
+
+    moveRight() {
+        this.imgPlayer.style.left = parseInt(this.imgPlayer.style.left) + 10 + 'px';
+    }
+}
