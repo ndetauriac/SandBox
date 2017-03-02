86298f513ff2ddc583766dc668e636ba17d098a2
ndetauriac <nicolas.detauriac@gmail.com>
2017-03-02 17:03:43 +0100

    Bug Fix NaN Coins
    Fix the NaN value in coin total

--- Gaming/js/Items/Coin.js
+++ Gaming/js/Items/Coin.js
@@ -5,6 +5,10 @@ class Coin extends Item
         super(x, y, stasis);
         this.sprite = new Sprites("./images/Coin.png", 10, 1, true, 3);
         this.amount = 10;
+    }
 
+    get Amount()
+    {
+        return this.amount;
     }
 }
