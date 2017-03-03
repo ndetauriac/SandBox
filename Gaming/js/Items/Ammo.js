ad4703b12718bf520ed1ca1a70a92bff85e6eba4
ndetauriac <nicolas.detauriac@gmail.com>
2017-03-03 02:48:07 +0100

    Preload img for better perf Change behavior damage text: add and reset animation when reapplying same damage type
    Signed-off-by: ndetauriac <nicolas.detauriac@gmail.com>

--- Gaming/js/Items/Ammo.js
+++ Gaming/js/Items/Ammo.js
@@ -3,7 +3,7 @@ class Ammo extends Item
     constructor(x, y, stasis = false)
     {
         super(x, y , stasis);
-        this.sprite = new Sprites("./images/shurikenItem.png", 10, 1, true, 3);
+        this.sprite = new Sprites("shurikenItem", 10, 1, true, 3);
         this.amount = 10;
         this.itemType = "Ammo";
     }
--- Gaming/js/Items/Coin.js
+++ Gaming/js/Items/Coin.js
@@ -3,7 +3,7 @@ class Coin extends Item
     constructor(x, y, stasis = false)
     {
         super(x, y, stasis);
-        this.sprite = new Sprites("./images/coin.png", 10, 1, true, 3);
+        this.sprite = new Sprites("coin", 10, 1, true, 3);
         this.amount = 10;
         this.itemType = "Coin";
     }
