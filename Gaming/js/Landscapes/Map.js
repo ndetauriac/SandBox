fcd78438e82524cc3beab54cd22e19eaf4a844fe
ndetauriac <nicolas.detauriac@gmail.com>
2017-05-19 14:59:45 +0200

    Add files via upload

--- Gaming/js/Landscapes/Map.js
+++ Gaming/js/Landscapes/Map.js
@@ -4,12 +4,12 @@ class Map
     {
         this.context2D = document.getElementById('gameArea').getContext('2d');
 
-        this.sizeMapX = WIN_WIDTH * 4;
+        this.sizeMapX = WIN_WIDTH * 2;
         this.sizeMapY = WIN_HEIGHT * 1;
-        this.width = Math.min(this.sizeMapX, WIN_WIDTH);
-        this.height = Math.min(this.sizeMapY, WIN_HEIGHT);
-        this.background = bibliImages["whiteWorld"];
-        this.initMap();
+        this.backgrounds = [];
+        this.backgrounds.push(new Background(this.sizeMapX, this.sizeMapY, "whiteWorld_scn"));
+        this.backgrounds.push(new Background(this.sizeMapX, this.sizeMapY, "Clouds"));
+        this.backgrounds.push(new Background(this.sizeMapX, this.sizeMapY, "game-background_first"));
     }
 
     get MapX()
@@ -24,33 +24,12 @@ class Map
 
     draw()
     {
-        var posX = posWorldX * this.factorX;
-        var posY = posWorldY * this.factorY;
-
-        this.context2D.drawImage(this.background, posX, posY, this.bgW, this.bgH, 0, 0, this.width/WIN_RATIO, this.height/WIN_RATIO);
+        this.backgrounds.forEach(function(elt) {
+            elt.draw();
+        });
+        
+        //this.context2D.drawImage(this.background_2, posX_2, posY_2, this.bgW, this.bgH, 0, 0, this.width/WIN_RATIO, this.height/WIN_RATIO);
+        //this.context2D.drawImage(this.background_1, posX_1, posY_1, this.bgW, this.bgH, 0, 0, this.width/WIN_RATIO, this.height/WIN_RATIO);
         //this.context2D.drawImage(this.background, posX, posY, this.background.width, this.background.height, 0, 0, this.background.width/WIN_RATIO, this.background.height/WIN_RATIO);
-    }
-
-    initMap()
-    {
-        if (this.sizeMapX != WIN_WIDTH)
-            this.factorX = (this.background.width - WIN_WIDTH) / (this.sizeMapX - WIN_WIDTH) * WIN_RATIO;
-        else
-            this.factorX = 0;
-
-        if (this.sizeMapY != WIN_HEIGHT)
-            this.factorY = (this.background.height - WIN_HEIGHT) / (this.sizeMapY - WIN_HEIGHT) * WIN_RATIO;
-        else
-            this.factorY = 0;
-
-        if(this.width > this.sizeMapX)
-            this.bgW = this.width;
-        else
-            this.bgW = this.width;
-
-        if(this.height > this.sizeMapY)
-            this.bgH = this.height;
-        else
-            this.bgH = this.height;
     }
 }
